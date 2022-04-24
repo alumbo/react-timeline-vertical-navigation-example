409d618d8c56769e03cdaceedbb060e8b2659df1
Matthieu Chavigny <matthieu.chavigny@zoi.com>
2022-04-24 11:20:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ItemsGrid,
+  TimelineVerticalNavigation,
+} from "react-timeline-vertical-navigation";
 
 function App() {
+  const getRandomDate = () => {
+    const start = new Date(2000, 0, 1);
+    const end = new Date();
+    return new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime())
+    );
+  };
+  const dates = [];
+  for (let i = 0; i < 50; i++) {
+    dates.push(getRandomDate());
+  }
+  const timelineVerticalNavigation = React.createRef();
+  const itemsGrid = React.createRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TimelineVerticalNavigation
+        ref={timelineVerticalNavigation}
+        dates={dates}
+        onDateSelected={(event) => {
+          itemsGrid.current.scrollToDate(event.detail);
+        }}
+      />
+      <ItemsGrid
+        ref={itemsGrid}
+        dates={dates}
+        onScrolledToDate={(event) => {
+          const firstDateVisible = event.detail;
+          timelineVerticalNavigation.current.updateSelectedDate(
+            firstDateVisible
+          );
+        }}
+      />
+    </>
   );
 }
 
